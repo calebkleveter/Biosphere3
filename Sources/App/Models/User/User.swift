cf2df91d87d1153ff49eceec605ba104434c971a
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-30 15:57:57 -0500

    Added email property to User model

--- Sources/App/Models/User/User.swift
+++ Sources/App/Models/User/User.swift
@@ -4,18 +4,22 @@ import PostgreSQLProvider
 final class User: Model {
     let storage: Storage = Storage()
     let username: String
+    let email: String
     
-    init(username: String) {
+    init(username: String, email: String) {
         self.username = username
+        self.email = email
     }
     
     init(row: Row) throws {
         self.username = try row.get("username")
+        self.email = try row.get("email")
     }
     
     func makeRow() throws -> Row {
         var row = Row()
         try row.set("username", username)
+        try row.set("email", email)
         return row
     }
 }
