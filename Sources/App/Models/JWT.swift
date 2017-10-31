bcda7164419c23b8bf711dc248bf06d7eca02f77
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-31 12:59:56 -0500

    Created JWTConfig.signer constant

--- Sources/App/Models/JWT.swift
+++ Sources/App/Models/JWT.swift
@@ -3,4 +3,5 @@ import JWT
 struct JWTConfig {
     static let signerKey = "smokey-atmosphere-38"
     static let headers = JSON(["type": "JWT", "alg": "HS256"])
+    static let signer = HS256(bytes: JWTConfig.signerKey.bytes)
 }
