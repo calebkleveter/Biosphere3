365b66f64cba342e3d307ec7b2aa6a4cfd87e4b5
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-31 13:00:29 -0500

    Created JWTConfig.expirationTime constant

--- Sources/App/Models/JWT.swift
+++ Sources/App/Models/JWT.swift
@@ -4,4 +4,5 @@ struct JWTConfig {
     static let signerKey = "smokey-atmosphere-38"
     static let headers = JSON(["type": "JWT", "alg": "HS256"])
     static let signer = HS256(bytes: JWTConfig.signerKey.bytes)
+    static let expirationTime = 1000
 }
