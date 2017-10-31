af65463abfb929768806c0aea17098539d77a504
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-31 12:20:55 -0500

    Installed AuthProvider package

--- Package.swift
+++ Package.swift
@@ -12,10 +12,11 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
         .package(url: "https://github.com/vapor-community/postgresql-provider.git", .exact("2.1.0")),
-        .package(url: "https://github.com/vapor/jwt.git", .exact("2.3.0"))
+        .package(url: "https://github.com/vapor/jwt.git", .exact("2.3.0")),
+        .package(url: "https://github.com/vapor/auth-provider.git", .exact("1.2.0"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentProvider", "PostgreSQLProvider", "JWT"],
+        .target(name: "App", dependencies: ["Vapor", "FluentProvider", "PostgreSQLProvider", "JWT", "AuthProvider"],
                 exclude: [
                     "Config",
                     "Public",
