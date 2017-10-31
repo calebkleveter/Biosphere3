54ddc2eab8bd1b18880647b6de258febc6cb37c7
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-31 12:35:55 -0500

    Added password hashing to User.init

--- Sources/App/Models/User/User.swift
+++ Sources/App/Models/User/User.swift
@@ -1,21 +1,27 @@
 import PostgreSQLProvider
 
 final class User: Model {
+    static let hasher = BCryptHasher(cost: 10)
+    
     let storage: Storage = Storage()
     let username: String
     let email: String
     let password: String
     
-    init(username: String, email: String, password: String) {
+    init(username: String, email: String, password: String)throws {
+        let passwordBytes = password.makeBytes()
+        
         self.username = username
         self.email = email
-        self.password = password
+        self.password = try User.hasher.make(passwordBytes).makeString()
     }
     
     init(row: Row) throws {
+        let passwordBytes = try (row.get("password") as String).makeBytes()
+        
         self.username = try row.get("username")
         self.email = try row.get("email")
-        self.password = try row.get("password")
+        self.password = try User.hasher.make(passwordBytes).makeString()
     }
     
     func makeRow() throws -> Row {
