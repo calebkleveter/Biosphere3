a5f91e26d83961148831dd0e7d5b85c9e1391f5c
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-10-31 07:14:09 -0500

    Added password property to User model

--- Sources/App/Models/User/User.swift
+++ Sources/App/Models/User/User.swift
@@ -4,21 +4,25 @@ final class User: Model {
     let storage: Storage = Storage()
     let username: String
     let email: String
+    let password: String
     
-    init(username: String, email: String) {
+    init(username: String, email: String, password: String) {
         self.username = username
         self.email = email
+        self.password = password
     }
     
     init(row: Row) throws {
         self.username = try row.get("username")
         self.email = try row.get("email")
+        self.password = try row.get("password")
     }
     
     func makeRow() throws -> Row {
         var row = Row()
         try row.set("username", username)
         try row.set("email", email)
+        try row.set("password", password)
         return row
     }
 }
