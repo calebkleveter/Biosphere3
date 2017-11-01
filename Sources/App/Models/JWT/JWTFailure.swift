abc8fd104174666dcad06e230b6d4a2b4ed8a66d
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-01 17:08:00 -0500

    Documented JWTFailure enum

--- Sources/App/Models/JWT/JWTFailure.swift
+++ Sources/App/Models/JWT/JWTFailure.swift
@@ -1,13 +1,27 @@
 import JWT
 
+/// Failure cases when creating, verfiying, or fetching data from a JWT token.
 enum JWTFailure: Error, CustomStringConvertible {
+    
+    /// Signiture could not be verified.
     case signatureVerificationFailed
+    
+    /// Token issuer could not be verified.
     case issuerVerificationFailed
+    
+    /// The token has expired.
     case tokenIsExpired
+    
+    /// Payload could not be created.
     case payloadCreationFailed
+    
+    /// JWT token could not be created with the payload passed in.
     case createJWTFailed
+    
+    /// An error occured while getting data from a JWT payload.
     case payloadParsingFailed
     
+    /// A human readable message of the error case.
     var description: String {
         switch self {
         case .signatureVerificationFailed:
