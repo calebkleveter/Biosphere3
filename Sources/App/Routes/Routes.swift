97ae72864f92ca9faf53096706212e14738cf71e
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-01 16:52:45 -0500

    Commented contents of Droplet.setupRoutes method

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -2,6 +2,8 @@ import Vapor
 
 extension Droplet {
     func setupRoutes() throws {
+        // Register the routes from the custom controllers
+        // With the current droplet
         UserController(drop: self).configureRoutes()
         JWTController(drop: self).configureRoutes()
     }
