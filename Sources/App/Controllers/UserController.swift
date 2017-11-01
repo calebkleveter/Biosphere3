b0f0bd02c25da7759cbc65acb4604b8f2e39499a
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-01 06:28:43 -0500

    Created /user/create route

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -8,6 +8,23 @@ final class UserController {
     }
     
     func configureRoutes() {
-        
+        let user = drop.grouped("user")
+        user.post("create", handler: createUser)
+    }
+    
+    func createUser(_ request: Request)throws -> ResponseRepresentable {
+        guard let username = request.data["username"]?.string,
+              let password = request.data["password"]?.string,
+              let email = request.data["email"]?.string else {
+                return try JSON(node: ["error", "Bad request. Missing required data"])
+        }
+        do {
+            let user = try User(username: username, email: email, password: password)
+            try user.save()
+            let token = try user.jwt()
+            return try JSON(node: ["token", token])
+        } catch let error {
+            return try JSON(node: ["error": error])
+        }
     }
 }
