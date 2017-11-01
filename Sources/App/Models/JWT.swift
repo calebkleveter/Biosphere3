8a4c6d41f80f0278bd4d5980ffc32ae4153159bb
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-01 06:48:46 -0500

    Created JWT.canVerifySigniture static method

--- Sources/App/Models/JWT.swift
+++ Sources/App/Models/JWT.swift
@@ -6,3 +6,13 @@ struct JWTConfig {
     static let signer = HS256(bytes: JWTConfig.signerKey.bytes)
     static let expirationTime = 1000
 }
+
+extension JWT {
+    static func canVerifySigniture(fromToken token: String, withSigner signer: String) -> Bool {
+        do {
+            let receivedJWT = try JWT(token: token)
+            try receivedJWT.verifySignature(using: HS256(key: signer.bytes))
+            return true
+        } catch { return false }
+    }
+}
