2af8c951368b4872afddbe5e66b7a95a39fc0f2d
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-01 17:03:23 -0500

    Commented UserController class

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -8,31 +8,42 @@ final class UserController {
     }
     
     func configureRoutes() {
+        // Add public routes to 'user' droplet route group.
         let user = drop.grouped("user")
         user.post("create", handler: create)
         
+        // Add protected routes to protected 'user' droplet route group.
         let protected = user.grouped(JWTMiddleware())
         protected.get("me", handler: me)
     }
     
     func create(_ request: Request)throws -> ResponseRepresentable {
+        // Get data from request to create a new user.
         guard let username = request.data["username"]?.string,
               let password = request.data["password"]?.string,
               let email = request.data["email"]?.string else {
+                // Data required to create a new user is missing. Return an error.
                 return try JSON(node: ["error": "Bad request. Missing required data"])
         }
         do {
+            // Created a new user from the request data.
             let user = try User(username: username, email: email, password: password)
             try user.save()
+            
+            // Create a JWT token for the new user and return it.
             let token = try user.jwt()
             return try JSON(node: ["token": token])
         } catch let error {
+            // An error occured while creating the user or their JWT token. Return the error.
             return try JSON(node: ["error": error])
         }
     }
     
     func me(_ request: Request)throws -> ResponseRepresentable {
+        // Get the request's JWT token.
         let token = request.token
+        
+        // Get the email from the token's payload and return it.
         let email = try token.get(.email)
         return try JSON(node: ["email": email])
     }
