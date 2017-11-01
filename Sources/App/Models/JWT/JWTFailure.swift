7a0587d893165ff934b809ccea5d8c5b972511f6
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-01 09:45:10 -0500

    Created JWTFailure Error enum

---
+++ Sources/App/Models/JWT/JWTFailure.swift
@@ -0,0 +1,24 @@
+import JWT
+
+enum JWTFailure: Error, CustomStringConvertible {
+    case signatureVerificationFailed
+    case issuerVerificationFailed
+    case tokenIsExpired
+    case payloadCreationFaile
+    case createJWTFailed
+    
+    var description: String {
+        switch self {
+        case .signatureVerificationFailed:
+            return "Could not verify signature"
+        case .issuerVerificationFailed:
+            return "Could not verify JWT issuer"
+        case .tokenIsExpired:
+            return "Your token is expired"
+        case .payloadCreationFaile:
+            return "Error creating JWT payload"
+        case .createJWTFailed:
+            return "Error creating JWT token"
+        }
+    }
+}
