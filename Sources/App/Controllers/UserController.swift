96239ab60a8c9eb954f2a8cd1c0745c45df160b3
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-01 06:39:24 -0500

    Fixed user create responses that returned arrays to return objects

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -16,13 +16,13 @@ final class UserController {
         guard let username = request.data["username"]?.string,
               let password = request.data["password"]?.string,
               let email = request.data["email"]?.string else {
-                return try JSON(node: ["error", "Bad request. Missing required data"])
+                return try JSON(node: ["error": "Bad request. Missing required data"])
         }
         do {
             let user = try User(username: username, email: email, password: password)
             try user.save()
             let token = try user.jwt()
-            return try JSON(node: ["token", token])
+            return try JSON(node: ["token": token])
         } catch let error {
             return try JSON(node: ["error": error])
         }
