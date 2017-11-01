3d3aafd476c55c656cfdbb1229ebeeeb66febef8
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-01 16:59:33 -0500

    Commented JWTController class

--- Sources/App/Controllers/JWTController.swift
+++ Sources/App/Controllers/JWTController.swift
@@ -9,18 +9,24 @@ final class JWTController {
     }
     
     func configureRoutes() {
+        // Add contoller routes to 'token' droplet group.
         let token = drop.grouped("token")
         token.get("verify", handler: verify)
     }
     
     func verify(_ request: Request)throws -> ResponseRepresentable {
+        // Get the JWT token from the request object.
         guard let jwt = request.headers["Authorization"]?.string else {
+            // 'Authorization' header does not exist. Return an error.
             return try JSON(node: ["error": "Bad request. Missing required header"])
         }
         do {
+            // Verify the JWT token.
             try jwt.verify()
+            // Verification succeded. Return true.
             return try JSON(node: ["verified": true])
         } catch {
+            // Verification failed. Return false.
             return try JSON(node: ["verified": false])
         }
     }
