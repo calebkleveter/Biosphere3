49a1edf1f7b37ed00add1d236b3d175711f03fc6
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-01 06:59:27 -0500

    Added JWTController routes to Droplet.setupRoutes

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -3,5 +3,6 @@ import Vapor
 extension Droplet {
     func setupRoutes() throws {
         UserController(drop: self).configureRoutes()
+        JWTController(drop: self).configureRoutes()
     }
 }
