df7b403fb8a6f2d267758c423dc8909631ce02a2
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-01 17:09:22 -0500

    Documented Request.token computed variable

--- Sources/App/Models/JWT/Request+JWT.swift
+++ Sources/App/Models/JWT/Request+JWT.swift
@@ -1,6 +1,7 @@
 import HTTP
 
 extension Request {
+    /// Get the JWT token held in a request's 'Authorization' header.
     var token: String {
         if let token = self.headers["Authorization"]?.string {
             return token
