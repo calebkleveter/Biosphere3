1161c1367d604d8221c8646a5fb049bde1db7b51
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-03 11:05:14 -0500

    Created Config.preparePivots extension method

--- Sources/App/Models/Pivots/Pivots.swift
+++ Sources/App/Models/Pivots/Pivots.swift
@@ -1,3 +1,10 @@
+import Vapor
 import Fluent
 
 typealias UserProject = Pivot<User, Project>
+
+extension Config {
+    func preparePivots() {
+        self.preparations.append(UserProject.self)
+    }
+}
