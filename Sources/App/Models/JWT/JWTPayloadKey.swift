5f8ae9e6be94b5444146936e68e7758432de86c3
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-04 18:08:11 -0500

    Created JWTPayloadKey.username enum key

--- Sources/App/Models/JWT/JWTPayloadKey.swift
+++ Sources/App/Models/JWT/JWTPayloadKey.swift
@@ -1,11 +1,13 @@
 enum JWTPayloadKey {
     case email
+    case username
     case id
     case expiration
     
     var key: String {
         switch self {
         case .email: return "userEmail"
+        case .username: return "userUsername"
         case .id: return "userId"
         case .expiration: return "exp"
         }
