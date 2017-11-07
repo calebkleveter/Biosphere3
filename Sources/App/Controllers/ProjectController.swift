5bef747ddc85017a01dd84b4698d9065dd14a6b5
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-07 12:24:54 -0600

    Created ProjectController.configureRoutes method

--- Sources/App/Controllers/ProjectController.swift
+++ Sources/App/Controllers/ProjectController.swift
@@ -6,4 +6,9 @@ final class ProjectController {
     init(drop: Droplet) {
         self.drop = drop
     }
+    
+    func configureRoutes() {
+        // Add controller routes to `project` droplet route group.
+        let project = drop.grouped("project").grouped(APIErrorMiddleware())
+    }
 }
