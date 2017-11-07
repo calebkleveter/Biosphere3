119a2f3f47c21a4e2a44ad49290db0df1e3c7a39
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-07 16:53:37 -0600

    Created Abort.missingJWTToken error

--- Sources/App/Models/Abort/Abort.swift
+++ Sources/App/Models/Abort/Abort.swift
@@ -3,4 +3,5 @@ import Vapor
 extension Abort {
     static let missingRequestData = Abort(.badRequest, reason: "Missing data in request body")
     static let badUserID = Abort(.badRequest, reason: "Unable to find user for ID")
+    static let missingJWTToken = Abort(.badRequest, reason: "Missing Authorization header")
 }
