2e75966c2d9b7ea125aa415cd5593486c2591597
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-07 12:26:44 -0600

    Configured all ProjectController routes with the application droplet

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -4,6 +4,7 @@ extension Droplet {
     func setupRoutes() throws {
         // Register the routes from the custom controllers
         // With the current droplet
+        ProjectController(drop: self).configureRoutes()
         UserController(drop: self).configureRoutes()
         JWTController(drop: self).configureRoutes()
     }
