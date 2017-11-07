3be77c3139d1bad774ae53fb820eeeb5b1b216b2
Caleb Kleveter <caleb.kleveter@gmail.com>
2017-11-07 16:31:56 -0600

    Created /project/create protected route

--- Sources/App/Controllers/ProjectController.swift
+++ Sources/App/Controllers/ProjectController.swift
@@ -10,5 +10,35 @@ final class ProjectController {
     func configureRoutes() {
         // Add controller routes to `project` droplet route group.
         let project = drop.grouped("project").grouped(APIErrorMiddleware())
+        
+        // Add private controller routes to `protected` droplet route group
+        let protected = project.grouped(JWTMiddleware())
+        protected.post("create", handler: create)
+    }
+    
+    func create(_ request: Request)throws -> ResponseRepresentable {
+        // Get data needed for finding a user and creating a project.
+        guard let name = request.data["name"]?.string,
+              request.token != "",
+              let tokenId = try Int(request.token.get(.id))
+              else {
+                // Data missing. Abort.
+                throw Abort.missingRequestData
+        }
+        
+        // Created an identifier from the tokenId and get the matching user.
+        let id: Identifier = Identifier.number(.int(tokenId))
+        guard let user = try User.find(id) else {
+            throw Abort.badUserID
+        }
+        
+        // Create a project with the name and user that were found
+        let project = Project(name: name, user: user)
+        try project.save()
+        return try JSON(node:
+            ["data":
+                ["name": project.name,
+                 "user_id": user.id ?? "null"]
+            ])
     }
 }
